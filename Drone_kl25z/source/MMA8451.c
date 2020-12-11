1a7cc99f26123a50b4e8ab8860d6c7f579c512a8
Madera <Madera@DESKTOP-1NIONB2>
2020-12-11 00:23:26 -0600

    Se limpio el codigo del Acelerometro en su propio .h y .c

---
+++ Drone_kl25z/source/MMA8451.c
@@ -0,0 +1,58 @@
+/******************************************************************************
+* Accelerometer initialization function
+******************************************************************************/
+#include "MMA8451.h"
+#include "I2C.h"
+
+unsigned char Xoffset, Yoffset, Zoffset;
+
+void Accelerometer_Init (void)
+{
+	unsigned char reg_val = 0;
+
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG2, 0x40);		// Reset all registers to POR values
+
+	do		// Wait for the RST bit to clear
+	{
+		reg_val = I2C_ReadRegister(MMA845x_I2C_ADDRESS, CTRL_REG2) & 0x40;
+	} 	while (reg_val);
+
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, XYZ_DATA_CFG_REG, 0x00);		// +/-2g range -> 1g = 16384/4 = 4096 counts
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG2, 0x02);		// High Resolution mode
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG1, 0x3D);	// ODR = 1.56Hz, Reduced noise, Active mode
+}
+
+/******************************************************************************
+* Simple offset calibration
+******************************************************************************/
+
+void Calibrate (void)
+{
+	unsigned char reg_val = 0;
+
+	while (!reg_val)		// Wait for a first set of data
+	{
+		reg_val = I2C_ReadRegister(MMA845x_I2C_ADDRESS, STATUS_REG) & 0x08;
+	}
+
+	I2C_ReadMultiRegisters(MMA845x_I2C_ADDRESS, OUT_X_MSB_REG, 6, AccData);		// Read data output registers 0x01-0x06
+
+	Xout_14_bit = ((short) (AccData[0]<<8 | AccData[1])) >> 2;		// Compute 14-bit X-axis output value
+	Yout_14_bit = ((short) (AccData[2]<<8 | AccData[3])) >> 2;		// Compute 14-bit Y-axis output value
+	Zout_14_bit = ((short) (AccData[4]<<8 | AccData[5])) >> 2;		// Compute 14-bit Z-axis output value
+
+	Xoffset = Xout_14_bit / 8 * (-1);		// Compute X-axis offset correction value
+	Yoffset = Yout_14_bit / 8 * (-1);		// Compute Y-axis offset correction value
+	Zoffset = (Zout_14_bit - SENSITIVITY_2G) / 8 * (-1);		// Compute Z-axis offset correction value
+
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG1, 0x00);		// Standby mode to allow writing to the offset registers
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, OFF_X_REG, Xoffset);
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, OFF_Y_REG, Yoffset);
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, OFF_Z_REG, Zoffset);
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG3, 0x00);		// Push-pull, active low interrupt
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG4, 0x01);		// Enable DRDY interrupt
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG5, 0x01);		// DRDY interrupt routed to INT1 - PTA14
+	I2C_WriteRegister(MMA845x_I2C_ADDRESS, CTRL_REG1, 0x3D);		// ODR = 1.56Hz, Reduced noise, Active mode
+}
+
+
